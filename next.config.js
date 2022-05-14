cf9bc2915cacbd7cf0eb7ec3ba7645da96d90502
williamcsa04 <williamcaetano14@gmail.com>
2022-05-14 01:41:42 -0300

    :construction: Added logic to get most champions images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_DDRAGON_API_BASE: 'https://ddragon.leagueoflegends.com/cdn/',
+    NEXT_PUBLIC_DDRAGON_API_CHAMPIONS: 'data/pt_BR/champion.json',
+    NEXT_PUBLIC_DDRAGON_API_VERSION: '12.6.1',
+    NEXT_PUBLIC_DDRAGON_API_SPLASH: 'img/champion/splash/',
+    NEXT_PUBLIC_DDRAGON_API_LOADING: 'img/champion/loading/',
+    NEXT_PUBLIC_DDRAGON_API_SQUARE: 'img/champion/',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
+import { Flex, Image } from '@chakra-ui/react';
 import Head from 'next/head';
 import { ddragonServices } from '../services';
 
-export default function Home({ champions }) {
+export default function Home({ champions, URL }) {
   return (
     <div>
       <Head>
         <title>Competitive Pool</title>
       </Head>
+      <Flex>
+        {Object.values(champions).map((champion, index) => {
+          return (
+            <Image
+              key={index}
+              src={`${URL.GET_LOADING}${champion.name}_0.jpg`}
+              alt={champion.name}
+            />
+          );
+        })}
+      </Flex>
     </div>
   );
 }
 
-export async function getStaticProps(context) {
-  const champions = await (await ddragonServices.getChampions()).json();
+export async function getStaticProps() {
+  const { data: champions } = await (
+    await ddragonServices.getChampions()
+  ).json();
   return {
     props: {
       champions,
+      URL: ddragonServices.URL,
     },
   };
 }
