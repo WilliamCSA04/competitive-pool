9c61e65a77b5974b181cd64bef71620ded87d8ce
williamcsa04 <williamcaetano14@gmail.com>
2022-05-13 22:44:10 -0300

    :heavy_plus_sign: Added chakra ui

---
+++ styles/styles.js
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  50: '#e4e4ff',
+  100: '#b2b3ff',
+  200: '#7f80ff',
+  300: '#4c4dff',
+  400: '#1a1aff',
+  500: '#0000e6',
+  600: '#0000b4',
+  700: '#000082',
+  800: '#000050',
+  900: '#000021',
+};
+
+export const theme = extendTheme({ colors });
