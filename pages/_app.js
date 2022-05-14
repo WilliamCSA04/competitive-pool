bb987068b4b6834e4dc1f494efa5cd52094ddbb3
williamcsa04 <williamcaetano14@gmail.com>
2022-05-13 22:56:18 -0300

    :heavy_plus_sign: Added react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Hydrate, QueryClientProvider } from 'react-query';
 import { theme } from '../styles';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
