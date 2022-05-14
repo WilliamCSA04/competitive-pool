343fdf2d6d65166e69afd5043d3a5ca06e5981b0
williamcsa04 <williamcaetano14@gmail.com>
2022-05-13 22:04:56 -0300

    :tada: Started project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Competitive Pool</title>
+        <meta name="description" content="Website made to compile some data" />
+      </Head>
+    </div>
+  );
+}
