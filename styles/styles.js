dc7406402198dbe3b837de7df4152e6bf897bb8c
williamcsa04 <williamcaetano14@gmail.com>
2022-05-16 22:56:17 -0300

    :adhesive bandage: Fixed colors

--- styles/styles.js
+++ styles/styles.js
@@ -2,16 +2,18 @@ import { extendTheme } from '@chakra-ui/react';
 import { cardStyle } from '../components';
 
 const colors = {
-  50: '#e4e4ff',
-  100: '#b2b3ff',
-  200: '#7f80ff',
-  300: '#4c4dff',
-  400: '#1a1aff',
-  500: '#0000e6',
-  600: '#0000b4',
-  700: '#000082',
-  800: '#000050',
-  900: '#000021',
+  brand: {
+    50: '#e4e4ff',
+    100: '#b2b3ff',
+    200: '#7f80ff',
+    300: '#4c4dff',
+    400: '#1a1aff',
+    500: '#0000e6',
+    600: '#0000b4',
+    700: '#000082',
+    800: '#000050',
+    900: '#000021',
+  },
 };
 
 const components = {
