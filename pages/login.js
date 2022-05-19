b08e9b2b6a26dc7093a673de4080bd96a258e578
williamcsa04 <williamcaetano14@gmail.com>
2022-05-19 19:28:09 -0300

    :construction: login page

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,24 @@
-import {
-  Button,
-  Center,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  VStack,
-} from '@chakra-ui/react';
+import { Button, Center, Heading, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 import { Card } from '../components';
+import { supabase } from '../utils';
 
 export default function Login() {
+  const [hasError, setHasError] = useState(false);
+  console.log(hasError);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { error } = supabase.auth.signIn({
+      provider: 'github',
+    });
+    setHasError(error);
+  };
+
   return (
-    <Center as="form" h="100%">
+    <Center as="form" h="100%" onSubmit={onSubmit}>
       <Card as={VStack} spacing="5" maxW="300px">
-        <Heading>Login</Heading>
-        <FormControl id="email">
-          <FormLabel>Email</FormLabel>
-          <Input type="email" />
-        </FormControl>
-        <Button>Receive Magic Link</Button>
+        <Heading mr="auto">Login</Heading>
+        <Button type="submit">Login with github</Button>
       </Card>
     </Center>
   );
