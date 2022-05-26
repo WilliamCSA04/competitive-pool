530df41cd5e45fa93736761f1ad79d02bb3b55f0
williamcsa04 <williamcaetano14@gmail.com>
2022-05-26 00:05:48 -0300

    :sparkles: Created contants

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const ASSETS_PATHS = {
+  ROLES: {
+    TOP: '/Position_Master-Top.png',
+    JUNGLE: '/Position_Master-Jungle.png',
+    MID: '/Position_Master-Mid.png',
+    ADC: '/Position_Master-Bot.png',
+    SUP: '/Position_Master-Support.png',
+  },
+};
--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,3 @@
 export { supabase } from './supabase';
 export { PUBLIC_VARS } from './config';
+export * from './constants';
